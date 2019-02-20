a582606904bf615d63ad07427c05ba90fcda126b
hala-almaimoni <halaalmaimoni@gmail.com>
2019-02-20 16:04:51 +0300

    Updates

---
+++ precache-manifest.73573028b2b33ac825a79b8a33678127.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ToDoList/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ab8f91d15bd732f56403e712e30ed7d3",
+    "url": "/ToDoList/static/media/background.ab8f91d1.jpg"
+  },
+  {
+    "revision": "ca9d33dca053b8c7bab6",
+    "url": "/ToDoList/static/js/runtime~main.ca9d33dc.js"
+  },
+  {
+    "revision": "3179ad0faf73d54d64f9",
+    "url": "/ToDoList/static/js/main.3179ad0f.chunk.js"
+  },
+  {
+    "revision": "6d24ecdf0e034bcfa371",
+    "url": "/ToDoList/static/js/2.6d24ecdf.chunk.js"
+  },
+  {
+    "revision": "3179ad0faf73d54d64f9",
+    "url": "/ToDoList/static/css/main.e3111222.chunk.css"
+  },
+  {
+    "revision": "7496348fcd920c5fdf4a5e9bc01a665e",
+    "url": "/ToDoList/index.html"
+  }
+];
